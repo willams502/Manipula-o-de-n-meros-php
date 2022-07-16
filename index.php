1902a71e5816bb9ddee2540f3a5a4cd2c8c7c3e1
Willams <84189241+willams502@users.noreply.github.com>
2022-07-16 12:08:36 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    //Manipulações de números
+    $valor =  8;  
+    $valor1 = 5.75;
+    $valor3 = "teste";
+    //    $valor_convertido = (int) $valor;
+    /*php tenta prever oque ta fazendo de merda para tentar corrigir --- mas pode ser um problema*/ 
+    // (ins_int) pergunta se a variavel é int ou seja a função (ins_****) pegunta se a variável é aquele valor.
+    var_dump(is_int($valor1));
+
+
+
+
+?>
